82b414979ad4f31511d179e1af76f97f60647955
reyraa <haghighatkhah.ali@gmail.com>
2021-06-28 01:40:57 +0200

    Create the Safari web extension project

---
+++ safari/ReduxMonitor/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  ReduxMonitor
+//
+//  Created by Ali Haghighatkhah on 27.06.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ safari/ReduxMonitor/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  ReduxMonitor
+//
+//  Created by Ali Haghighatkhah on 27.06.21.
+//
+
+import Cocoa
+import SafariServices.SFSafariApplication
+import SafariServices.SFSafariExtensionManager
+
+let appName = "ReduxMonitor"
+let extensionBundleIdentifier = "com.tangent.ReduxMonitor.Extension"
+
+class ViewController: NSViewController {
+
+    @IBOutlet var appNameLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.appNameLabel.stringValue = appName
+        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: extensionBundleIdentifier) { (state, error) in
+            guard let state = state, error == nil else {
+                // Insert code to inform the user that something went wrong.
+                return
+            }
+
+            DispatchQueue.main.async {
+                if (state.isEnabled) {
+                    self.appNameLabel.stringValue = "\(appName)'s extension is currently on."
+                } else {
+                    self.appNameLabel.stringValue = "\(appName)'s extension is currently off. You can turn it on in Safari Extensions preferences."
+                }
+            }
+        }
+    }
+    
+    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
+            guard error == nil else {
+                // Insert code to inform the user that something went wrong.
+                return
+            }
+
+            DispatchQueue.main.async {
+                NSApplication.shared.terminate(nil)
+            }
+        }
+    }
+
+}
